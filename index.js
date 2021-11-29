88bea2d83813e7d0a2fc97750ee2724c59ab11bd
Abdiel Batista dos Santos <abdielb@yahoo.com>
2021-11-28 22:28:24 -0300

    config mongodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //config inicial
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
 
 //ler JSON
 app.use(
@@ -16,5 +18,24 @@ app.get('/',(req,res)=>{
     res.json({message: "Oi Humano!"})
 })
 
-//port
-app.listen(3000)
+
+//gera.48Gd$RH9_W
+
+//mongodb+srv://abdi:gera.48Gd$RH9_W@apicluster.aewac.mongodb.net/banco-api?retryWrites=true&w=majority
+
+//porta
+
+dotenv.config();
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+mongoose
+.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.aewac.mongodb.net/banco-api?retryWrites=true&w=majority`
+    )
+
+.then(()=>{
+    console.log('Conectado')
+    app.listen(3000)
+}).catch((err)=> console.log(err));
+
